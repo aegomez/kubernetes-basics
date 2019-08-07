0868935f9ea476252a8fa15d204c6136152a0715
Adrian Gomez <aegomez@tekmexico.com>
2019-08-07 11:19:33 -0700

    Updated part 6 and README

--- README.md
+++ README.md
@@ -8,8 +8,15 @@ Run the scripts or follow their included commands in this order.
 2. Deploy an app using kubectl
 3. Explore the app
 4. Expose the app publicly
-    * Create the service
+    * Create a service
     * Use labels
     * Delete the service
-5. 
-6. 
+5. Scale the app
+    * Scale up
+    * Load balancing
+    * Scale down
+6. Update the app
+    * Update the app image
+    * Verify the update
+    * Failure during update
+    * Rollback an update
--- scripts/6-update.sh
+++ scripts/6-update.sh
@@ -7,7 +7,7 @@ log()
   echo -e "\033[31m\n*** $1 ...\033[0m"
 }
 
-###  i) Update the version of the app  ###
+##  i) Update the version of the app  ###
 
 
 # OPTIONAL: list deployments and pods
@@ -17,65 +17,66 @@ log()
 # OPTIONAL: view the current image version
 # kubectl describe pods
 
-# log "Updating the app\'s image to version 2"
-## kubectl set image TYPE_NAME CONTAINER_N=IMAGE_N
-# kubectl set image \
-#   deployments/kubernetes-bootcamp \
-#   kubernetes-bootcamp=jocatalin/kubernetes-bootcamp:v2
 
-# sleep 3
+log "Updating the app\'s image to version 2"
+# kubectl set image TYPE_NAME CONTAINER_N=IMAGE_N
+kubectl set image \
+  deployments/kubernetes-bootcamp \
+  kubernetes-bootcamp=jocatalin/kubernetes-bootcamp:v2
 
-# log "Listing updated pods"
-# kubectl get pods
+sleep 3
 
-
-###  (ii) Verify the update  ###
+log "Listing updated pods"
+kubectl get pods
 
 
-# log "Displaying Service description"
-# kubectl describe services/kubernetes-bootcamp
-
-# log "Getting the node port"
-# NODE_PORT=$(kubectl get services/kubernetes-bootcamp \
-#   -o go-template='{{(index .spec.ports 0).nodePort}}')
-# echo NODE_PORT=$NODE_PORT
-
-# log "Trying to request the port six times"
-# for i in {1..6}
-# do
-#   sleep 1
-#   log "Request $i/6"
-#   curl $(minikube ip):$NODE_PORT
-# done
-
-# log "Trying to verify update using rollout status"
-# kubectl rollout status deployments/kubernetes-bootcamp
-
-# log "Displaying Pod description to confirm current image"
-# kubectl describe pods
+##  (ii) Verify the update  ###
 
 
-# ###  (iii) Update failure  ###
+log "Displaying Service description"
+kubectl describe services/kubernetes-bootcamp
+
+log "Getting the node port"
+NODE_PORT=$(kubectl get services/kubernetes-bootcamp \
+  -o go-template='{{(index .spec.ports 0).nodePort}}')
+echo NODE_PORT=$NODE_PORT
+
+log "Trying to request the port six times"
+for i in {1..6}
+do
+  sleep 1
+  log "Request $i/6"
+  curl $(minikube ip):$NODE_PORT
+done
+
+log "Trying to verify update using rollout status"
+kubectl rollout status deployments/kubernetes-bootcamp
+
+log "Displaying Pod description to confirm current image"
+kubectl describe pods
 
 
-# log "Trying to update image to version 10"
-# kubectl set image \
-#   deployments/kubernetes-bootcamp \
-#   kubernetes-bootcamp=gcr.io/google-samples/kubernetes-bootcamp:v10
-
-# sleep 3
-
-# log "Listing deployment status"
-# kubectl get deployments
-
-# log "Listing Pods"
-# kubectl get pods
-
-# log "Something went wrong, displaying pod desciption"
-# kubectl describe pods
+###  (iii) Update failure  ###
 
 
-# ###  (iv) Rollback the update  ###
+log "Trying to update image to version 10"
+kubectl set image \
+  deployments/kubernetes-bootcamp \
+  kubernetes-bootcamp=gcr.io/google-samples/kubernetes-bootcamp:v10
+
+sleep 3
+
+log "Listing deployment status"
+kubectl get deployments
+
+log "Listing Pods"
+kubectl get pods
+
+log "Something went wrong, displaying pod desciption"
+kubectl describe pods
+
+
+###  (iv) Rollback the update  ###
 
 
 log "Rolling back to previous version"
